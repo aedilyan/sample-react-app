48db897ff73e174ed5c5a00c3b5b0588e40202c1
aedilyan <you@example.comaedilyan@gmail.com>
2019-10-29 18:24:19 +0400

    jest

--- src/components/common/input/Input.test.js
+++ src/components/common/input/Input.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { act } from 'react-dom/test-utils';
-import Image from './Image';
+import Input from './Input';
 
-let container, testSrc = 'https://www.benivo.com/hubfs/Benivo-Apr2017/Images/favicon-72.png';
+let container;
 
 beforeEach(() => {
     container = document.createElement('div');
@@ -18,8 +18,8 @@ afterEach(() => {
 it('can render and update a Input', () => {
     // Test first render and effect
     act(() => {
-        ReactDOM.render(<Image src={testSrc} />, container);
+        ReactDOM.render(<Input />, container);
     });
-    const inputNode = container.querySelector('img');
-    expect(inputNode.src).toBe(testSrc);
+    const inputNode = container.querySelector('input');
+    expect(inputNode.type).toBe('text');
 });
