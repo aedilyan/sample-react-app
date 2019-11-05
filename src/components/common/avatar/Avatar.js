04a7374f514c2e84fc3f6edbc7ff64bf1d4dc0e7
aedilyan <aedilyan@gmail.com>
2019-11-05 22:46:29 +0400

    added react-skeleton

--- src/components/common/avatar/Avatar.js
+++ src/components/common/avatar/Avatar.js
@@ -1,18 +1,29 @@
-import React from "react"
-import Image from '../image/Image'
+import React, { useEffect, useState } from "react"
+import { Image } from 'Components'
+import { window } from 'Utils'
+import Skeleton from 'react-skeleton-loader'
 import './avatar.css'
 
 //don't render if input props are the same - React.memo
 const Avatar = React.memo(({ imageSrc, name, favorite }) => {
 
     console.log(`Avatar ${name} rendered - thx to memo!`) // eslint-disable-line no-undef
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState('');
+
+    useEffect(() => {
+        window.setTimeout(() => {
+            setDescription(name)
+            setImage(imageSrc)
+        }, 1000)
+    }, [name, imageSrc])
 
     return (
         <React.Fragment>
-            <p className="description">{name || 'guest'}
+            <p className="description">{description || <Skeleton count={1} />}
                 {favorite && <span> | my favorite</span>}
             </p>
-            <Image src={imageSrc} className="avatar" alt="Avatar" />
+            {image ? <Image src={image} className="avatar" alt="Avatar" /> : <Skeleton count={1} borderRadius={'20px'} width={'70px'} height={'50px'} />}
         </React.Fragment>
     );
 });
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import { lazy } from 'react'
 export { default as Heading } from 'Common/heading/Heading'
 export { default as Avatar } from 'Common/avatar/Avatar'
 export { default as Button } from 'Common/button/Button'
+export { default as Image } from 'Common/image/Image'
 export { HeartButton } from 'Common/heartButton/HeartButton'
 
 const Product = lazy(() => import('Common/product/Product'))
